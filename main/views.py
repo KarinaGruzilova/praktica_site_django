d7c3c7b252873bef18be78c58913eabb033f202e
Karina dmitrievna <karinkaagruzilova@gmail.com>
2025-01-25 01:07:02 +0400

    The main app is ready, pages index and about/ 2:43

--- main/views.py
+++ main/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': True
+        'title': 'Home - Главная',
+        'content': 'Главная ',
     }
     return render(request, 'main/index.html',context)
 
 def about(request):
-    return HttpResponse('Home page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас ',
+        'text_on_page': 'Магазин классный'
+    }
+    return render(request, 'main/about.html',context)
