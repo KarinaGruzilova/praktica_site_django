de0b6d537e765d0c856265b39e912645bdb39702
Karina dmitrievna <karinkaagruzilova@gmail.com>
2025-01-29 19:59:45 +0400

    Full text search well-done!

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 
@@ -82,10 +83,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'magazin',
+        'USER': 'magazin',
+        'PASSWORD': 'magazin',
+        'HOST': 'localhost',
+        'PORT': '5432',  # Default is 5432
     }
 }
 
